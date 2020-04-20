e9e9d6a5c99806234b82c531523a0cb203730644
motiya <54768914+mtiyz@users.noreply.github.com>
2020-04-20 18:10:09 +0900

    :sparkles: add install Homebrew

--- bin/install.sh
+++ bin/install.sh
@@ -16,25 +16,11 @@ function choise {
 }
 
 function start () {
-  if choise "Do you want to start install for $1?"; then
-    echo "Start"
+  if choise "Do you want to start install"; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
   else
     echo "Quit" && exit 0
   fi
 }
 
-if [ -f /etc/os-release ]; then
-  OS_RELEASE=`cat /etc/os-release`
-  case $OS_RELEASE in
-    *ubuntu* ) start "Ubuntu";;
-    *debian* ) start "Debian";;
-    *centos* ) start "CentOS";;
-    *fedora* ) start "Fedora";;
-    *rhel* ) start "RHEL";;
-    *arch* ) start "ArchLinux";;
-    *opensuse* ) start "OpenSUSE";;
-    * ) echo "Sorry, We don't support this distribution" && exit 1;;
-  esac
-else
-  echo "Sorry, We don't support this distribution" && exit 1
-fi
+start
